eddcf1e0e25d4ec9341b1c07b25e25f43168e781
HermanBach <herman.gashkov@gmail.com>
2023-03-27 21:02:38 +0500

    тест

---
+++ Controllers/MaltController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace beerCreator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaltController : ControllerBase
+    {
+        // GET: api/<MaltController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Ok";
+        }
+
+    }
+}
