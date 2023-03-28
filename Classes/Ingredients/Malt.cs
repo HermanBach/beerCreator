791c52b2885bfa2f30d61fd0b4ceaae7761026ad
HermanBach <herman.gashkov@gmail.com>
2023-03-28 08:51:57 +0500

    Update Malt.cs

--- Classes/Ingredients/Malt.cs
+++ Classes/Ingredients/Malt.cs
@@ -2,5 +2,13 @@
 {
     public class Malt : Ingredient
     {
+        /// <summary>
+        /// Цветность солода European Brewing Convention
+        /// </summary>
+        public string Color { get; set; }
+        /// <summary>
+        /// Экстракт на с.в. солода
+        /// </summary>
+        public double Extract { get; set; }
     }
 }
