c87bbd542e3400011a46976d9c29cb7940dd5337
HermanGashkov <herman.gashkov@gmail.com>
2023-07-14 08:49:12 +0500

    убрал TODO

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -8,9 +8,7 @@ namespace beerCreator.Controllers
     [ApiController]
     public class AdminController : ControllerBase
     {
-            //TODO: вынести ConnectionString в appsettings.json
         readonly MaltRepository maltRepo = new(new Program().GetConnectionString());
-
         readonly HopRepository hopRepo = new(new Program().GetConnectionString());
 
         [HttpGet("CreateTables")]
