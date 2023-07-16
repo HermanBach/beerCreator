4cf40aa8c896ee6b316b4721d820347b2461c153
HermanBach <herman.gashkov@gmail.com>
2023-07-16 19:20:37 +0500

    Добавил контроллер Yeast

---
+++ Controllers/YeastController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using beerCreator.Classes.Ingredients;
+using beerCreator.Models;
+
+namespace beerCreator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YeastController : ControllerBase
+    {
+
+        readonly YeastRepository rep = new(new Program().GetConnectionString());
+
+        [HttpGet("GetAllYeast")]
+        public List<Yeast> GetAllYeast()
+        {
+            return rep.GetAllYeasts();
+        }
+
+        [HttpGet("GetYeastById")]
+        public Yeast GetYeastById(int id)
+        {
+            return rep.GetYeastById(id);
+        }
+
+        [HttpPost("CreaneNewYeast")]
+        public void CreaneNewYeast(Yeast yeast)
+        {
+            rep.CreaneNewYeast(yeast);
+
+        }
+
+        [HttpPost("EditYeast")]
+        public void EditHop(Yeast yeast)
+        {
+            rep.EditYeast(yeast);
+        }
+        [HttpDelete("DeleteYeast")]
+        public void DeleteHop(int id)
+        {
+            rep.DeleteYeast(id);
+        }
+    }
+}
