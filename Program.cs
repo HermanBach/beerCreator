a71fc5fd41ff2e02ace0bb9df31a21f1e68a3e4e
HermanBach <herman.gashkov@gmail.com>
2023-08-15 20:40:02 +0500

    Добавил в CORS возможность принимать запросы от приложения на vue 3

--- Program.cs
+++ Program.cs
@@ -2,8 +2,19 @@ public class Program
 {
     public static void Main(string[] args)
     {
+
+        var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         var builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(name: MyAllowSpecificOrigins,
+                              policy =>
+                              {
+                                  policy.WithOrigins("http://localhost:5173");
+                              });
+        });
         // Add services to the container.
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
@@ -11,6 +22,7 @@ public class Program
 
         var app = builder.Build();
 
+        
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
@@ -19,6 +31,8 @@ public class Program
         }
 
         app.UseHttpsRedirection();
+
+        app.UseCors(MyAllowSpecificOrigins);
 
         app.UseAuthorization();
 
