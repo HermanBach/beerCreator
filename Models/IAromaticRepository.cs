26528ca041c559f02ba0328a29471126501829e0
HermanBach <herman.gashkov@gmail.com>
2023-09-20 13:17:11 +0500

    Реализовал репозиторий Aromatic

--- Models/IAromaticRepository.cs
+++ Models/IAromaticRepository.cs
@@ -1,4 +1,7 @@
 ﻿using beerCreator.Classes.Ingredients;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace beerCreator.Models
 {
@@ -10,5 +13,80 @@ namespace beerCreator.Models
         void EditAromatic(Aromatic aromatic);
         void DeleteAromatic(long Id);
         void CteateNewTable();
+    }
+    public class AromaticRepository : IAromaticRepository
+    {
+        string? connectionString = null;
+        public AromaticRepository(string conStr)
+        {
+            connectionString = conStr;
+        }
+        public List<Aromatic> GetAllAromatic()
+        {
+            string sqlQuery = "SELECT * FROM Aromatic";
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return db.Query<Aromatic>(sqlQuery).ToList();
+            }
+        }
+
+        public Aromatic GetAromaticById(long Id)
+        {
+            string sqlQuery = "SELECT * FROM Aromatic WHERE Id = @Id";
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return db.Query(sqlQuery, new { Id }).FirstOrDefault();
+            }
+        }
+        public void CreateNewAromatic(Aromatic aromatic)
+        {
+            string sqlQuery =
+                "INSERT INTO Aromatic (Name, Description, AlphaAcid, HopType)" +
+                "VALUES (@Name, @Description)";
+
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                db.Execute(sqlQuery, aromatic);
+
+            }
+        }
+        public void EditAromatic(Aromatic aromatic)
+        {
+            string sqlQuery =
+                "UPDATE Aromatic " +
+                "SET Name = @Name, " +
+                "Description = @Description, " +
+                "WHERE Id = @Id";
+
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                db.Execute(sqlQuery, aromatic);
+            }
+
+        }
+        public void DeleteAromatic(long Id)
+        {
+            string sqlQuery =
+                "DELETE FROM Aromatic " +
+                "WHERE Id = @Id";
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                db.Execute(sqlQuery, new { Id });
+            }
+        }
+        public void CteateNewTable()
+        {
+            string sqlQuery =
+                "CREATE TABLE Aromatic(" +
+                "Id bigint IDENTITY(1, 1) PRIMARY KEY," +
+                "Name varchar(255) NOT NULL," +
+                "Description varchar(500)," +
+                ");";
+
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                db.Execute(sqlQuery);
+            }
+        }
     }
 }
